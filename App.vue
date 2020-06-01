30f4badcb659646f80fc8fad3977b67853bc11ae
David Moseler <davidmoseler@protonmail.com>
2020-05-31 23:51:18 -0300

    Add App.vue

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <paginator
+    initialPage = 1
+    pageCount = 5
+    initialPageSize = 10
+  />
+</template>
+
+<script>
+  import 'materialize-css'
+  import 'materialize-css/dist/css/materialize.css'
+  import 'material-design-icons/iconfont/material-icons.css'
+  import paginator from './src/components/paginator'
+
+  export default {
+    components: {
+      paginator
+    }
+  }
+</script>
