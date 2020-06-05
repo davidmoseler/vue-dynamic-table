26fe6286c7db219724a457cb27d06446d2c7de5f
David Moseler <davidmoseler@protonmail.com>
2020-06-04 21:10:31 -0300

    Add button component

---
+++ src/components/btn.vue
@@ -0,0 +1,46 @@
+<template>
+  <a
+    class="btn btn-sec btn-rounded"
+    :class='[color, textColor]'
+    @click="$emit('click', $event)"
+  >
+    {{text}}
+  </a>
+</template>
+
+<script>
+  import 'materialize-css'
+  import 'materialize-css/dist/css/materialize.css'
+
+  export default {
+    props: ['text', 'color'],
+    computed: {
+      textColor: function(){
+        if(this.color == 'white'){
+          return 'grey-text'
+        } else {
+          return 'white-text'
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .btn{
+    text-transform: uppercase;
+  }
+
+  .btn-sec {
+    color: darkgrey;
+    font-weight: bold;
+    font-size: 14px;
+    border: solid 1px;
+    border-color: lightgray;
+    background-color: white;
+  }
+
+  .btn-rounded {
+    border-radius: 50px!important
+  }
+</style>
